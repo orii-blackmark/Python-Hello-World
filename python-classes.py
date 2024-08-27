e88abaabe2b4fd1b6f4b376a5a90270157e5bf51
oriel blackmark <kiplangatoriel@gmail.com>
2024-08-28 01:46:37 +0300

    Constructor methods

--- python-classes.py
+++ python-classes.py
@@ -2,9 +2,16 @@ class MyComplexNumber:
     # Constructor methods
     def __init__(self, real = 0, imag = 0):
         print("MyComplexNumber constructor executing...")
-        self.real_path = real
+        self.real_part = real
         self.imag_part = imag
         
     def displayComplex(self):
-        print("{0} + {1}j".format(self.real_part,self.image_part))    
+        print("{0} + {1}oriel".format(self.real_part,self.imag_part))    
         
+        
+# create a new object against MyComplexNumber class
+cmplx1 = MyComplexNumber(40,50)
+
+# calling displayComplex() function
+cmplx1.displayComplex()        
+
