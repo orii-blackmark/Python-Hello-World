8304be84c59b6ff2c3683fc51cf46d443b46d78d
oriel blackmark <kiplangatoriel@gmail.com>
2024-08-28 01:20:14 +0300

    Constants and variables

--- test.py
+++ test.py
@@ -8,3 +8,19 @@ print(PI)
 a = "Apple"
 print(a)
 
+# Changing  the value of a variable
+a = "Aeroplane"
+print(a)
+
+a = 100
+print(a)
+
+# Assigning Multiple Values to a variable
+b,c,d = 1,2.5, "hello"
+print(b,c,d)
+
+#Assigning same value to multiple variables 
+b=c=d=5
+print(b,c,d)
+
+
