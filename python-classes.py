300f2279161338acc0dcafa7874d01ebb9a805ac
oriel blackmark <kiplangatoriel@gmail.com>
2024-08-28 01:57:00 +0300

    Done with classes and objects

--- python-classes.py
+++ python-classes.py
@@ -15,3 +15,17 @@ cmplx1 = MyComplexNumber(40,50)
 # calling displayComplex() function
 cmplx1.displayComplex()        
 
+# create another object against myComplexNumber class
+# and create a new attribute 'new_attribute'
+cmplx2 = MyComplexNumber(60,70)
+cmplx2.new_attribute =80 
+
+print(cmplx2.real_part, cmplx2.imag_part, cmplx2.new_attribute)
+# output (60,70,80)
+
+# Deleting object attribute and the object 
+print(cmplx1)
+del cmplx1.real_part
+del cmplx1
+
+print(cmplx1)
