26fa3a1e197d9cdf5887618972eda393e3d0c3fb
oriel blackmark <kiplangatoriel@gmail.com>
2024-08-28 01:02:35 +0300

    Modified - Declaring and assigning value to a variable

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
-print("hello world, This is my first project");
+# Declaring and assigning alue to constants
+
+PI = 3.14
+GRAVITY = 9.8
+print(PI)
+
+# Declaring and assigning value to a variable
+a = "Apple"
+print(a)
+
